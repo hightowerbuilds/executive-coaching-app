fc533a7212bb03636442c86cc6065eca4fb62b24
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-03-15 20:26:59 -0700

    march15.3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,12 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className="footContainer">
-        <div className='contactButton'>CONTACT</div>
-        <div className='contactButton'>CONTACT</div>
-        <div className='contactButton'>CONTACT</div>
-        <div className='contactButton'>CONTACT</div>
+        <div className='footButton'>CONTACT</div>
+        <div className='footButton'>CONTACT</div>
+        <div className='footButton'>CONTACT</div>
+        <div className='footButton'>CONTACT</div>
+        <div className='footButton'>CONTACT</div>
+        <div className='footButton'>CONTACT</div>
     </div>
   )
 }
